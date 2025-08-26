653c9d93183c6fc153272e5b0e4cd9fcca255cd8
Blesson <blessonthomas2005@gmail.com>
2025-08-27 02:17:09 +0530

    update

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+x=1
+y=2
 def hello(request):
-    return render(request,'hello.html',{'dic':'Blesson'})
+    return render(request,'hello.html',{'dic':'Paramashivam'})
