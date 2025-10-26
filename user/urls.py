2cf12dcf2c02318d54412c79fa7caaa7b53d734d
Blesson <blessonthomas2005@gmail.com>
2025-10-27 01:21:28 +0530

    updated

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 
+#the place that awak function and stuff like is we get into hello/book it will run the function bookscript
+
 #url config
 urlpatterns=[
     path("",views.chinadan),
     path('notes/',views.getnote),
+    path("notes/seeall/",views.getnotes),
+    path("notes/create",views.createnote),
     path('hello/',views.bookspirit),
     path('hello/',views.chinadan),
     path('hello/book/',views.bookspirit)
--- user/views.py
+++ user/views.py
@@ -9,16 +9,35 @@ from .serializers import user_serializer
 from django.http import HttpResponse
 # Create your views here.
 
+#here we keep the logic and the function and stuff
 
 def chinadan(request):
+
     return render(request,'hello.html',{'dic':'Paramashivam'})
     
 
 def bookspirit(request):
     return render(request,'user/book.html')    
 
+#giving one input  manualy and seeing it
 @api_view (['GET'])
 def getnote(request):
     serializer = user_serializer(data={"note": 3, "date": "hello"})
     serializer.is_valid()  # optional for static data
     return Response(serializer.data)
+
+#to see all the items in the database
+@api_view (['GET'])
+def getnotes(request):
+    notes = user.objects.all() #getting all the data from the model (the DB ) user
+    serializer = user_serializer(notes,many = True)
+    #serializer.is_valid()  # optional for static data
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createnote(request):
+    serializer = user_serializer(data = request.data )
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
